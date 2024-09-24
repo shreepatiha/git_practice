a8d366202eda983942465a53edba07b6b25f8de0
shreepatiha <ashwanijhagena@gmail.com>
2024-09-24 09:51:00 +0530

    initial commit of the project

---
+++ src/main/java/org/jthread/Main.java
@@ -0,0 +1,13 @@
+package org.jthread;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int a=10;
+    int b=20;
+    int ans=a*b+b;
+    System.out.println(ans);
+
+    }
+}
